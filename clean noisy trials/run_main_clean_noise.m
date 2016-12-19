899246757d9e2978b6df53ff948f21b1fd96fb45
yairlak <yair.lakretz@gmail.com>
2016-12-19 12:23:22 +0200

    Add files via upload

--- clean noisy trials/run_main_clean_noise.m
+++ clean noisy trials/run_main_clean_noise.m
@@ -4,6 +4,5 @@ for patient = {'D011', '466', '469', '472'}
     settings.patient = patient{1}; 
     params = [];
     main_clean_noise
-    fprintf('%s\n', patient{1})
-    
+    fprintf('%s: %i trials omitted\n', patient{1}, sum(sum(results.omit_trials_mat_collapsed)))
 end
